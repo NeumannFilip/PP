17a42c5812ba9013aca3a160617d53096082e9ff
NeumannFilip <neumannfilip@icloud.com>
2025-03-20 23:23:36 +0100

    Camera as a pawn that require a lot of functionality as the most important foundation of this project. Total revamp, many todo

--- Source/PP/PPCamera.cpp
+++ Source/PP/PPCamera.cpp
@@ -5,29 +5,151 @@
 
 #include "InputActionValue.h"
 #include "Camera/CameraComponent.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 APPCamera::APPCamera()
 {
 
 	PrimaryActorTick.bCanEverTick = false;
+
+	//RootCollision = CreateDefaultSubobject<USphereComponent>("RootCollision");
+	//SetRootComponent(RootCollision);
+
+	TransformTarget = CreateDefaultSubobject<USceneComponent>("TransformTarget");
+	TransformTarget->SetupAttachment(RootCollision);
+
+	ArmRoot = CreateDefaultSubobject<USceneComponent>("ArmRoot");
 	
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
-	SpringArm->SetupAttachment(RootComponent);
-	SpringArm->TargetArmLength = 1000.0f; 
-	SpringArm->bDoCollisionTest = false; 
-	SpringArm->bInheritPitch = false;
-	SpringArm->bInheritRoll = false;
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(ArmRoot);
+	SpringArm->SetRelativeRotation(FRotator::MakeFromEuler(FVector(0, DefaultCameraArmAngle, 0)));
+	SpringArm->TargetArmLength = DefaultZoom;
+	SpringArm->bDoCollisionTest = false;
 	
-	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm);
+	Camera->SetRelativeRotation(FRotator::MakeFromEuler(FVector(0, DefaultCameraAngle, 0)));
+	Camera->FieldOfView = Fov;
 }
 
 
 void APPCamera::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	//if (USaveGameSubsystem::Get(this) && !USaveGameSubsystem::Get(this)->IsLoadingGame())
+	//{
+	//	DefaultRotation = GetActorRotation();
+	//}
+
+}
+
+void APPCamera::AddCameraMovementInput(FVector2D InputVector)
+{
+}
+
+void APPCamera::FocusOnActor(AActor* FocusedActor, bool bInstant)
+{	StopFocus();
+	if (bInstant)
+	{
+		//SetCameraLocationDirectly(FocusedActor->GetActorLocation());
+		return;
+	}
+	ActorToFocus = FocusedActor;
+	bFocusing = true;
+}
+
+void APPCamera::FocusOnLocation(FVector FocusedLocation, bool bInstant)
+{
+	StopFocus();
+	if (bInstant)
+	{
+		//SetCameraLocationDirectly(FocusedLocation);
+		return;
+	}
+	LocationToFocus = FocusedLocation;
+	bFocusing = true;
+}
+
+void APPCamera::StopFocus()
+{
+	bFocusing = false;
+	bAttachToFocusedActor = false;
+	ActorToFocus = nullptr;
+	if (AttachActor)
+	{
+		DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+		AttachActor = nullptr;
+	}
+}
+
+void APPCamera::AddExternalMovementInput(FVector2D InputVector)
+{
+	AddCameraMovementInput(InputVector);
+}
+
+void APPCamera::ResetRotationToDefault()
+{
+}
+
+void APPCamera::ResetZoomToDefault()
+{
+}
+
+void APPCamera::TeleportToLocation(FVector Location)
+{
+}
+
+void APPCamera::DigitalRotateCamera(float Input)
+{
+}
+
+void APPCamera::ZoomCamera(float Input)
+{
+}
+
+float APPCamera::GetZoom() const
+{
+}
+
+float APPCamera::GetZoomFraction() const
+{
+}
+
+void APPCamera::SetDirectMouseControlMode(bool bEnabled)
+{
+}
+
+void APPCamera::MovementInputTriggered(const FInputActionValue& ActionValue)
+{
+}
+
+void APPCamera::DigitalRotateInputTriggered(const FInputActionValue& ActionValue)
+{
+}
+
+void APPCamera::MouseRotateSwitchStarted()
+{
+}
+
+void APPCamera::MouseRotateSwitchCompleted()
+{
+}
+
+void APPCamera::ZoomInputTriggered(const FInputActionValue& ActionValue)
+{
+}
+
+void APPCamera::MoveCursorOutOfEdge()
+{
+}
+
+FVector2D APPCamera::CalculateEdgeScrollVector()
+{
+	const FVector2D EdgeScrollVector = CalculateEdgeScrollVector();
+
+	StopFocus();
+	AddCameraMovementInput(EdgeScrollVector);
 }
 
 // Called every frame
@@ -37,29 +159,19 @@ void APPCamera::Tick(float DeltaTime)
 
 }
 
-void APPCamera::MoveCamera(const FInputActionValue& Value)
+void APPCamera::Serialize(FArchive& Ar)
 {
-	FVector2D Movement = Value.Get<FVector2D>();
-	FVector NewLocation = GetActorLocation();
-	NewLocation.X += Movement.X * MoveSpeed * GetWorld()->GetDeltaSeconds();
-	NewLocation.Y += Movement.Y * MoveSpeed * GetWorld()->GetDeltaSeconds();
-	SetActorLocation(NewLocation);
-}
+	Super::Serialize(Ar);
+	if (!Ar.IsSaveGame())
+	{
+		return;
+	}
+	FTransform Transform = GetActorTransform();
+	Ar << Transform;
 
-void APPCamera::ZoomCamera(const FInputActionValue& Value)
-{
-	float ZoomDelta = Value.Get<float>();
-	float NewArmLength = SpringArm->TargetArmLength + ZoomDelta * ZoomSpeed * GetWorld()->GetDeltaSeconds();
-	NewArmLength = FMath::Clamp(NewArmLength, MinZoom, MaxZoom);
-	SpringArm->TargetArmLength = NewArmLength;
-}
-
-void APPCamera::RotateCamera(const FInputActionValue& Value)
-{
-	float RotationDelta = Value.Get<float>();
-	FRotator NewRotation = SpringArm->GetComponentRotation();
-	NewRotation.Yaw += RotationDelta * RotationSpeed * GetWorld()->GetDeltaSeconds();
-	SpringArm->SetWorldRotation(NewRotation);
+	Ar << ZoomTarget;
+	Ar << DefaultRotation;
+	Ar << SpringArm->TargetArmLength;
 }
 
 // Called to bind functionality to input
--- Source/PP/PPCamera.h
+++ Source/PP/PPCamera.h
@@ -7,6 +7,7 @@
 #include "GameFramework/Pawn.h"
 #include "PPCamera.generated.h"
 
+class USphereComponent;
 class USpringArmComponent;
 class UCameraComponent;
 class UInputMappingContext;
@@ -20,50 +21,165 @@ class PP_API APPCamera : public APawn
 public:
 	APPCamera();
 	
-	virtual void BeginPlay() override;
+	//default classes
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void Tick(float DeltaTime) override;
+	
+	virtual void Serialize(FArchive& Ar) override;
 
-	void MoveCamera(const FInputActionValue& Value);
-	void ZoomCamera(const FInputActionValue& Value); 
-	void RotateCamera(const FInputActionValue& Value);
+protected:
+	
+	virtual void BeginPlay() override;
+
+	void AddCameraMovementInput(FVector2D InputVector);
+
+	UFUNCTION(BlueprintCallable, meta=(AdvancedDisplay=bInstant))
+	void FocusOnActor(AActor* FocusedActor, bool bInstant = false);
+	UFUNCTION(BlueprintCallable, meta=(AdvancedDisplay=bInstant))
+	void FocusOnLocation(FVector FocusedLocation, bool bInstant = false);
+	UFUNCTION(BlueprintCallable)
+	void StopFocus();
+	UFUNCTION()
+	void AddExternalMovementInput(FVector2D InputVector);
+	UFUNCTION(BlueprintCallable)
+	void ResetRotationToDefault();
+	UFUNCTION(BlueprintCallable)
+	void ResetZoomToDefault();
+	UFUNCTION(BlueprintCallable)
+	void TeleportToLocation(FVector Location);
+	
+	UFUNCTION(BlueprintCallable)
+	void DigitalRotateCamera(float Input);
+	
+	UFUNCTION(BlueprintCallable)
+	void ZoomCamera(float Input);
+
+	UFUNCTION(BlueprintPure)
+	float GetZoom() const;
+	float GetZoomFraction() const;
 
 
+	UPROPERTY(BlueprintReadOnly, Category=Focus)
+	bool bFocusing{false};
+	UPROPERTY(BlueprintReadOnly, Category=Focus)
+	AActor* ActorToFocus;
+	UPROPERTY(BlueprintReadOnly, Category=Focus)
+	FVector LocationToFocus;
+	UPROPERTY(BlueprintReadOnly, Category=Focus)
+	bool bAttachToFocusedActor{false};
+	UPROPERTY(BlueprintReadOnly, Category=Focus)
+	AActor* AttachActor;
+
+	
+	//input
+	void SetDirectMouseControlMode(bool bEnabled);
+
+	UFUNCTION(Category="Input")
+	void MovementInputTriggered(const FInputActionValue& ActionValue);
+	UFUNCTION(Category="Input")
+	void DigitalRotateInputTriggered(const FInputActionValue& ActionValue);
+	UFUNCTION(Category="Input")
+	void MouseRotateSwitchStarted();
+	UFUNCTION(Category="Input")
+	void MouseRotateSwitchCompleted();
+	UFUNCTION(Category="Input")
+	void ZoomInputTriggered(const FInputActionValue& ActionValue);
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input")
+	UInputAction* DigitalRotateAction;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input")
+	UInputAction* MouseRotateSwitchAction;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input")
+	UInputAction* ZoomAction;
+
+	
+	
 private:
 
+	void MoveCursorOutOfEdge();
+
+	FVector2D CalculateEdgeScrollVector();
+
+	UPROPERTY()
+	float ZoomTarget;
+	UPROPERTY()
+	bool DirectMouseControlActive;
+	//For temporary disabling edge scrolling during gameplay
+	UPROPERTY()
+	bool bEdgeScrollEnabled = true;
+	UPROPERTY()
+	FRotator DefaultRotation;
+	UPROPERTY()
+	float ZoomCache = -1;
+
+
+	//Components
 	UPROPERTY(VisibleAnywhere, Category="Camera")
 	USpringArmComponent* SpringArm;
 	UPROPERTY(VisibleAnywhere, Category="Camera")
 	UCameraComponent* Camera;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Camera")
+	USphereComponent* RootCollision;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Camera")
+	USceneComponent* TransformTarget;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Camera")
+	USceneComponent* ArmRoot;
 
-	//settings
-	UPROPERTY(EditAnywhere, Category = "Camera")
-	float MoveSpeed = 500.0f;
 
-	UPROPERTY(EditAnywhere, Category = "Camera")
-	float ZoomSpeed = 100.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Camera")
-	float RotationSpeed = 50.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Camera")
-	float MinZoom = 500.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Camera")
-	float MaxZoom = 3000.0f;
-	//Input
+	//setup
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Camera")
+	float Fov{70};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Zoom")
+	FVector2D ZoomRange{200, 5000};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Zoom", meta = (EditCondition = "false"))
+	float DefaultZoom{2900};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Transform")
+	FVector2D DefaultCameraPitchZoomRange{0.65, 0.8};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Transform")
+	float DefaultCameraArmAngle{-50};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Transform")
+	float DefaultCameraAngle{4.75};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Transform")
+	FVector2D CameraPitchClamp{-30, -70};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Transform")
+	float CameraPitchFreeAngle{2.5};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Transform")
+	float DefaultHeight{0};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	UCurveFloat* ZoomSpeedMultiplierCurve;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	float EdgeScrollSpeed{1.5};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	float DirectionalMovementSpeed{1.5};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	float MousePanSpeed{50};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	float DigitalRotationSpeed{125};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	float AnalogRotationSpeed{30};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Speed")
+	float ZoomSpeed{5000};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|InterpSpeed")
+	float TransformAnchorInterpSpeed{5};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|InterpSpeed")
+	float CameraPitchInterpSpeed{2};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|InterpSpeed")
+	float ZoomInterpSpeed{5};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|InterpSpeed")
+	float FocusSpeed{10};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup")
+	float EdgeScrollVerticalScreenSpacePercent{0.15};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup")
+	float GroundAdjustTraceLength{10000};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup")
+	float GroundAdjustInterpSpeed{1};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup")
+	TEnumAsByte<ETraceTypeQuery> GroundAdjustTraceChannel;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup")
+	float PitchDampeningMargin{5};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Setup|Audio")
+	FVector2D AudioListenerZoomRange{200, 5000};
 	
-	UPROPERTY(EditDefaultsOnly, Category="Input")
-	UInputMappingContext* InputMappingContext;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* MoveAction;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* ZoomAction;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* RotateAction;
-
-
 };
